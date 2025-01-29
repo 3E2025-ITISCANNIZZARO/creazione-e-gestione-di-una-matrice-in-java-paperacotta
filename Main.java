8a7e034cdd26cdc2a7b9388f0722210fbf652ba4
paperacotta <galati.daniele@itiscannizzaro.edu.it>
2025-01-29 11:30:23 +0100

    migliorato Main.java

--- Main.java
+++ Main.java
@@ -1,51 +1,50 @@
-// Esercizio: Lavorare con le Matrici in Java
-//
-// 1. Creare una matrice 3x3 di numeri interi.
-// 2. Popolare la matrice con valori casuali compresi tra 1 e 100.
-// 3. Implementare un metodo che calcoli la somma di tutti gli elementi della matrice.
-// 4. Implementare un metodo che trovi il valore massimo nella matrice.
-// 5. Stampare la matrice, la somma degli elementi e il valore massimo trovato.
-//
-// Suggerimento:
-// - Utilizza la classe Random per generare numeri casuali.
-// - Per stampare la matrice, usa un ciclo annidato.
+import java.util.Random;
 
 public class Main {
     public static void main(String[] args) {
-        // Creazione e popolamento della matrice
-        int[][] matrix = new int[3][3];
+        int[][] matrice = new int[3][3];
         Random random = new Random();
-        // COMPLETARE IL CODICE
-
-      
-
-        // Stampa della matrice 
+       
+        for (int riga = 0; riga < 3; riga++){
+            for (int colonna = 0; colonna < 3; colonna++){
+                matrice[riga][colonna] = (int) (Math.random() * 10 + 1);
+            }
+        }
+       
         System.out.println("Matrice:");
-        // COMPLETARE IL CODICE
-
-
-        // Calcolo della somma degli elementi
-        int sum = calculateSum(matrix);
-        System.out.println("Somma degli elementi: " + sum);
-
-        // Ricerca del valore massimo
-        int max = findMax(matrix);
-        System.out.println("Valore massimo: " + max);
+        for (int riga = 0; riga < 3; riga++){
+            for (int colonna = 0; colonna < 3; colonna++){
+                System.out.print(matrice[riga][colonna] + " ");
+            }
+            System.out.println();
+        }
+       
+        int somma = calcolaSomma(matrice);
+        System.out.println("Somma degli elementi: " + somma);
+       
+        int massimo = trovaMax(matrice);
+        System.out.println("Valore massimo: " + massimo);
     }
 
-    public static int calculateSum(int[][] matrix) {
-        int sum = 0;
-   
-// COMPLETARE IL CODICE
-      
-        return sum;
+    public static int calcolaSomma(int[][] matrice) {
+        int somma = 0;
+        for (int riga = 0; riga < 3; riga++){
+            for (int colonna = 0; colonna < 3; colonna++){
+                somma += matrice[riga][colonna];
+            }
+        }
+        return somma;
     }
 
-    public static int findMax(int[][] matrix) {
-        int max = matrix[0][0];
-
-// COMPLETARE IL CODICE
-      
-        return max;
+    public static int trovaMax(int[][] matrice) {
+        int massimo = matrice[0][0];
+        for (int riga = 0; riga < 3; riga++){
+            for (int colonna = 0; colonna < 3; colonna++){
+                if (matrice[riga][colonna] > massimo) {
+                    massimo = matrice[riga][colonna];
+                }
+            }
+        }
+        return massimo;
     }
 }
